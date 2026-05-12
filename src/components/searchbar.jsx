2b471b62da6ae4271f3fc2e7019e09e98dcea827
Aucire <jamesandaro0@gmail.com>
2026-05-12 10:41:48 +0300

    minor changes

--- src/components/searchbar.jsx
+++ src/components/searchbar.jsx
@@ -15,21 +15,25 @@ export const SearchBar = ({ searchedMovie, setSearchedMovie, fetchedData, setQue
 
     return (
         <>
-            <div className="searchbar flex justify-center items-center border border-gray-400 w-9/10 min-h-20 rounded-xl">
-                <input
-                    className="w-100 h-10 text-3xl outline-0 text-orange-600 font-bold"
-                    type="text"
-                    placeholder="Search for a movie"
-                    value={searchedMovie}
-                    onChange={handleChange}
-                />
-                <button onClick={handleClick}>🔍</button>
-            </div>
-            <div>
-                <section id="list">
-                    <MovieCard
-                        fetchedData={fetchedData} />
+            <div className="searchbar text-center border border-gray-400 w-9/10 min-h-20 rounded-xl">
+                <section className="searchdiv border border-gray-400 border-1 w-7/10 rounded-3xl">
+                    <input
+                        className="w-4/5 h-10 text-3xl outline-0 text-orange-600 font-bold"
+                        type="text"
+                        placeholder="Search for a movie"
+                        value={searchedMovie}
+                        onChange={handleChange}
+                    />
+                    <button onClick={handleClick}>🔍</button>                    
                 </section>
+
+            
+                <div>
+                    <section id="list">
+                        <MovieCard
+                            fetchedData={fetchedData} />
+                    </section>
+                </div>
             </div>
         </>
     )
